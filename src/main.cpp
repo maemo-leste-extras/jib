5f15f73e329bb690b3c2f3068abf96359660aa21
kroket <code@kroket.io>
2025-05-31 16:33:25 +0300

    clion_debug: move to lib/

--- src/main.cpp
+++ src/main.cpp
@@ -16,35 +16,13 @@ Q_IMPORT_PLUGIN(QXcbIntegrationPlugin)
 #include "mainwindow.h"
 #include "ctx.h"
 #include "windowmanager.h"
+#include "lib/clion_debug.h"
 
 int main(int argc, char *argv[]) {
   Q_INIT_RESOURCE(assets);
 
 #ifdef DEBUG
-  // For remote debugging (QEMU) with CLion, the environment variables need
-  // to be correctly set such that e.g. dbus will work. We can execute
-  // this hack to dump the environ to a file, then reads it below.
-  // This runs when `-D CMAKE_DEBUG_TYPE=Debug`
-
-  // /bin/sh -c 'nohup /tmp/tmp.uTGneVHiIM/cmake-build-debug/bin/jib >/dev/null 2>&1 &'; sleep 2; cat "/proc/`pidof jib`/environ" | tr "\0" "\n" > /home/user/env.sh; kill -9 "`pidof jib`"
-  setuid(1000);
-  QString path_env_file = "/home/user/env.sh";
-  qDebug() << "trying to read ENV from" << path_env_file << ", if it exists";
-  auto env_file = Utils::fileOpen(path_env_file);
-  for(auto &line: Utils::barrayToString(env_file).split("\n")) {
-    line = line.replace("export ", "");
-    int pos = line.indexOf("=");
-    auto key = line.left(pos);
-    auto val = line.remove(0, pos + 1);
-
-    if(val.startsWith("\""))
-      val = val.mid(1);
-    if(val.endsWith("\""))
-      val = val.mid(0, val.length() - 1);
-
-    if(val.isEmpty() || key.isEmpty()) continue;
-    qputenv(key.toStdString().c_str(), val.toStdString().c_str());
-  }
+  clion_debug_setup();
 #endif
 
   QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
